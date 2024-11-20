9e2b7dcae8abfcc49ae313060328c1ff2e51b44f
Raluca - Ștefania Rîșnoveanu (raluca@meetgeek.ai) <raluca@meetgeek.ai>
2024-11-20 17:46:47 +0200

    Removed output_language param + updated faster-whisper to the latest commit on master - 491852e1b9059de782d95b9b160b150cb0573c0f

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -329,8 +329,6 @@ def load_model(whisper_arch,
         "log_prob_low_threshold": None,
         # for compatibility with faster-whisper d57c5b40b06e59ec44240d93485a95799548af50
         "multilingual": False,
-        # for compatibility with faster-whisper d57c5b40b06e59ec44240d93485a95799548af50
-        "output_language": None
     }
 
     if asr_options is not None:
